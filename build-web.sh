7e0bcd88b2150a9d6f65512cd7f6e0e3626e0d2a
Igor Gonçalves <igor_goncalves@live.com>
2021-09-06 21:40:03 -0300

    fix(CI): web deploy - correção de diretorio base

--- build-web.sh
+++ build-web.sh
@@ -6,3 +6,4 @@ cd ../br_ipti_tag_app
 flutter packages pub get
 flutter config --enable-web
 flutter build web
+sed -E 's/\ <base href="\/">/<base href="\/br_ipti_tag_app\/">/' web/index.html > tmpfile ; mv tmpfile web/index.html 
