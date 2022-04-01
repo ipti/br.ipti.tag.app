1522a69f257ec3f1549de9d1973c74e00ea65c23
Lucas Silva <lucasre.silva@gmail.com>
2022-04-01 05:48:17 -0300

    feat(School): tabs add

---
+++ br_ipti_tag_app/lib/app/features/school/presentation/widgets/tabs/school_adress_tab.dart
@@ -0,0 +1,13 @@
+import 'package:br_ipti_tag_app/app/features/school/presentation/widgets/forms/school_basic_data_form.dart';
+import 'package:flutter/material.dart';
+
+class AddressTab extends StatelessWidget {
+  const AddressTab({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SchoolBasicDataForm();
+  }
+}
---
+++ br_ipti_tag_app/lib/app/features/school/presentation/widgets/tabs/school_reports_tab.dart
@@ -0,0 +1,13 @@
+import 'package:br_ipti_tag_app/app/features/school/presentation/widgets/forms/school_basic_data_form.dart';
+import 'package:flutter/material.dart';
+
+class ReportsTab extends StatelessWidget {
+  const ReportsTab({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SchoolBasicDataForm();
+  }
+}
