a284324cc0b5df12f0f9ca180ffb7ea814559df5
Igor Gonçalves <igor_goncalves@live.com>
2022-04-18 19:51:10 -0300

    feat: alterando metodos para class para melhorar o desempenho

--- br_ipti_tag_app/lib/app/features/student/presentation/enrollment/form/filliation/_filiation_form_partial_page.dart
+++ br_ipti_tag_app/lib/app/features/student/presentation/enrollment/form/filliation/_filiation_form_partial_page.dart
@@ -35,47 +35,6 @@ class FiliationFormPageState extends State<FiliationFormPage> {
     const headingFiliation2 =
         Heading(text: "Mãe do aluno", type: HeadingType.Title3);
 
-    Widget inputName(String? name, Function(String) onChanged) => TagTextField(
-          label: "Nome",
-          hint: "Nome completo",
-          onChanged: onChanged,
-          value: name,
-          validator: requiredValidator,
-        );
-
-    Widget inputCPF(String? cpf, Function(String) onChanged) => TagTextField(
-          label: "CPF",
-          hint: "digite o CPF",
-          onChanged: onChanged,
-          formatters: [TagMasks.maskCPF],
-          value: cpf,
-          validator: requiredValidator,
-        );
-
-    Widget inputRG(String? rg, Function(String) onChanged) => TagTextField(
-          label: "RG",
-          hint: "digite o número do RG",
-          onChanged: onChanged,
-          value: rg,
-          validator: requiredValidator,
-        );
-
-    Widget selectScholarity(int? classId, Function(int) onChanged) =>
-        TagDropdownField(
-          label: 'Escolaridade',
-          items: controller.scholarityItems,
-          value: classId,
-          onChanged: onChanged,
-        );
-
-    Widget inputJob(String? job, Function(String) onChanged) => TagTextField(
-          label: "Profissão",
-          hint: "Digite a profissão",
-          onChanged: onChanged,
-          value: job,
-          validator: requiredValidator,
-        );
-
     return BlocBuilder<EnrollmentFiliationBloc, EnrollmentFiliationState>(
         bloc: controller,
         builder: (context, state) {
@@ -92,28 +51,15 @@ class FiliationFormPageState extends State<FiliationFormPage> {
                     RowToColumn(
                       children: [
                         Flexible(
-                            child: inputName(
-                          state.nameResponsable,
-                          controller.setNameResponsable,
-                        )),
+                          child: _NameResponsableField(
+                            name: state.nameResponsable,
+                            controller: controller,
+                          ),
+                        ),
                         Flexible(
-                            child: inputCPF(
-                          state.cpfResponsable,
-                          controller.setCpfResponsable,
-                        )),
-                      ],
-                    ),
-                    RowToColumn(
-                      children: [
-                        Flexible(
-                            child: inputRG(
-                          state.rgResponsable,
-                          controller.setRgResponsable,
-                        )),
-                        Flexible(
-                          child: selectScholarity(
-                            state.scholarityResponsable,
-                            controller.setScholarityResponsable,
+                          child: _CpfResponsableField(
+                            cpf: state.cpfResponsable,
+                            controller: controller,
                           ),
                         ),
                       ],
@@ -121,8 +67,25 @@ class FiliationFormPageState extends State<FiliationFormPage> {
                     RowToColumn(
                       children: [
                         Flexible(
-                          child: inputJob(state.jobResponsable,
-                              controller.setJobResponsable),
+                            child: _RGResponsableField(
+                          rg: state.rgResponsable,
+                          controller: controller,
+                        )),
+                        Flexible(
+                          child: _ScholarityResponsableField(
+                            scholarity: state.scholarityResponsable,
+                            controller: controller,
+                          ),
+                        ),
+                      ],
+                    ),
+                    RowToColumn(
+                      children: [
+                        Flexible(
+                          child: _JobResponsableField(
+                            job: state.jobResponsable,
+                            controller: controller,
+                          ),
                         ),
                         const Spacer()
                       ],
@@ -135,28 +98,15 @@ class FiliationFormPageState extends State<FiliationFormPage> {
                     RowToColumn(
                       children: [
                         Flexible(
-                            child: inputName(
-                          state.nameFiliation1,
-                          controller.setNameFiliation1,
-                        )),
+                          child: _NameFilliation1Field(
+                            name: state.nameFiliation1,
+                            controller: controller,
+                          ),
+                        ),
                         Flexible(
-                            child: inputCPF(
-                          state.cpfFiliation1,
-                          controller.setCpfFiliation1,
-                        )),
-                      ],
-                    ),
-                    RowToColumn(
-                      children: [
-                        Flexible(
-                            child: inputRG(
-                          state.rgFiliation1,
-                          controller.setRgFiliation1,
-                        )),
-                        Flexible(
-                          child: selectScholarity(
-                            state.scholarityFiliation1,
-                            controller.setScholarityFiliation1,
+                          child: _CpfFilliation1Field(
+                            cpf: state.cpfFiliation1,
+                            controller: controller,
                           ),
                         ),
                       ],
@@ -164,8 +114,26 @@ class FiliationFormPageState extends State<FiliationFormPage> {
                     RowToColumn(
                       children: [
                         Flexible(
-                          child: inputJob(
-                              state.jobFiliation1, controller.setJobFiliation1),
+                          child: _RGFilliation1Field(
+                            rg: state.rgFiliation1,
+                            controller: controller,
+                          ),
+                        ),
+                        Flexible(
+                          child: _ScholarityFilliation1Field(
+                            scholarity: state.scholarityFiliation1,
+                            controller: controller,
+                          ),
+                        ),
+                      ],
+                    ),
+                    RowToColumn(
+                      children: [
+                        Flexible(
+                          child: _JobFilliation1Field(
+                            job: state.jobFiliation1,
+                            controller: controller,
+                          ),
                         ),
                         const Spacer()
                       ],
@@ -178,28 +146,15 @@ class FiliationFormPageState extends State<FiliationFormPage> {
                     RowToColumn(
                       children: [
                         Flexible(
-                            child: inputName(
-                          state.nameFiliation2,
-                          controller.setNameFiliation2,
-                        )),
+                          child: _NameFilliation2Field(
+                            name: state.nameFiliation2,
+                            controller: controller,
+                          ),
+                        ),
                         Flexible(
-                            child: inputCPF(
-                          state.cpfFiliation2,
-                          controller.setCpfFiliation2,
-                        )),
-                      ],
-                    ),
-                    RowToColumn(
-                      children: [
-                        Flexible(
-                            child: inputRG(
-                          state.rgFiliation2,
-                          controller.setRgFiliation2,
-                        )),
-                        Flexible(
-                          child: selectScholarity(
-                            state.scholarityFiliation2,
-                            controller.setScholarityFiliation2,
+                          child: _CpfFilliation2Field(
+                            cpf: state.cpfFiliation2,
+                            controller: controller,
                           ),
                         ),
                       ],
@@ -207,8 +162,26 @@ class FiliationFormPageState extends State<FiliationFormPage> {
                     RowToColumn(
                       children: [
                         Flexible(
-                          child: inputJob(
-                              state.jobFiliation2, controller.setJobFiliation2),
+                          child: _RGFilliation2Field(
+                            rg: state.rgFiliation2,
+                            controller: controller,
+                          ),
+                        ),
+                        Flexible(
+                          child: _ScholarityFilliation2Field(
+                            scholarity: state.scholarityFiliation2,
+                            controller: controller,
+                          ),
+                        ),
+                      ],
+                    ),
+                    RowToColumn(
+                      children: [
+                        Flexible(
+                          child: _JobFilliation2Field(
+                            job: state.jobFiliation2,
+                            controller: controller,
+                          ),
                         ),
                         const Spacer()
                       ],
@@ -231,5 +204,351 @@ class FiliationFormPageState extends State<FiliationFormPage> {
             ),
           );
         });
+  }
+}
+
+class _NameResponsableField extends StatelessWidget {
+  const _NameResponsableField({
+    Key? key,
+    this.name,
+    required this.controller,
+  }) : super(key: key);
+
+  final String? name;
+  final EnrollmentFiliationBloc controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return TagTextField(
+      key: const Key('STUDENT_ENROLLMENT_FILLIATION_RESPONSABLE_NAME'),
+      label: "Nome",
+      hint: "Nome completo",
+      onChanged: controller.setNameResponsable,
+      value: name,
+      validator: requiredValidator,
+    );
+  }
+}
+
+class _CpfResponsableField extends StatelessWidget {
+  const _CpfResponsableField({
+    Key? key,
+    this.cpf,
+    required this.controller,
+  }) : super(key: key);
+
+  final String? cpf;
+  final EnrollmentFiliationBloc controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return TagTextField(
+      key: const Key('STUDENT_ENROLLMENT_FILLIATION_RESPONSABLE_CPF'),
+      label: "CPF",
+      hint: "digite o CPF",
+      onChanged: controller.setCpfResponsable,
+      formatters: [TagMasks.maskCPF],
+      value: cpf,
+      validator: requiredValidator,
+    );
+  }
+}
+
+class _RGResponsableField extends StatelessWidget {
+  const _RGResponsableField({
+    Key? key,
+    this.rg,
+    required this.controller,
+  }) : super(key: key);
+
+  final String? rg;
+  final EnrollmentFiliationBloc controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return TagTextField(
+      key: const Key('STUDENT_ENROLLMENT_FILLIATION_RESPONSABLE_RG'),
+      label: "RG",
+      hint: "digite o número do RG",
+      onChanged: controller.setRgResponsable,
+      value: rg,
+      validator: requiredValidator,
+    );
+  }
+}
+
+class _ScholarityResponsableField extends StatelessWidget {
+  const _ScholarityResponsableField({
+    Key? key,
+    this.scholarity,
+    required this.controller,
+  }) : super(key: key);
+
+  final int? scholarity;
+  final EnrollmentFiliationBloc controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return TagDropdownField(
+      key: const Key('STUDENT_ENROLLMENT_FILLIATION_RESPONSABLE_SCHOLARITY'),
+      label: 'Escolaridade',
+      items: controller.scholarityItems,
+      value: scholarity,
+      onChanged: controller.setScholarityResponsable,
+    );
+  }
+}
+
+class _JobResponsableField extends StatelessWidget {
+  const _JobResponsableField({
+    Key? key,
+    this.job,
+    required this.controller,
+  }) : super(key: key);
+
+  final String? job;
+  final EnrollmentFiliationBloc controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return TagTextField(
+      key: const Key('STUDENT_ENROLLMENT_FILLIATION_RESPONSABLE_JOB'),
+      label: "Profissão",
+      hint: "Digite a profissão",
+      onChanged: controller.setJobResponsable,
+      value: job,
+    );
+  }
+}
+
+// Filliation 1
+
+class _NameFilliation1Field extends StatelessWidget {
+  const _NameFilliation1Field({
+    Key? key,
+    this.name,
+    required this.controller,
+  }) : super(key: key);
+
+  final String? name;
+  final EnrollmentFiliationBloc controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return TagTextField(
+      key: const Key('STUDENT_ENROLLMENT_FILLIATION_FILLIATION1_NAME'),
+      label: "Nome",
+      hint: "Nome completo",
+      onChanged: controller.setNameFiliation1,
+      value: name,
+      validator: requiredValidator,
+    );
+  }
+}
+
+class _CpfFilliation1Field extends StatelessWidget {
+  const _CpfFilliation1Field({
+    Key? key,
+    this.cpf,
+    required this.controller,
+  }) : super(key: key);
+
+  final String? cpf;
+  final EnrollmentFiliationBloc controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return TagTextField(
+      key: const Key('STUDENT_ENROLLMENT_FILLIATION_FILLIATION1_CPF'),
+      label: "CPF",
+      hint: "digite o CPF",
+      onChanged: controller.setCpfFiliation1,
+      formatters: [TagMasks.maskCPF],
+      value: cpf,
+      validator: requiredValidator,
+    );
+  }
+}
+
+class _RGFilliation1Field extends StatelessWidget {
+  const _RGFilliation1Field({
+    Key? key,
+    this.rg,
+    required this.controller,
+  }) : super(key: key);
+
+  final String? rg;
+  final EnrollmentFiliationBloc controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return TagTextField(
+      key: const Key('STUDENT_ENROLLMENT_FILLIATION_FILLIATION1_RG'),
+      label: "RG",
+      hint: "digite o número do RG",
+      onChanged: controller.setRgFiliation1,
+      value: rg,
+      validator: requiredValidator,
+    );
+  }
+}
+
+class _ScholarityFilliation1Field extends StatelessWidget {
+  const _ScholarityFilliation1Field({
+    Key? key,
+    this.scholarity,
+    required this.controller,
+  }) : super(key: key);
+
+  final int? scholarity;
+  final EnrollmentFiliationBloc controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return TagDropdownField(
+      key: const Key('STUDENT_ENROLLMENT_FILLIATION_FILLIATION1_SCHOLARITY'),
+      label: 'Escolaridade',
+      items: controller.scholarityItems,
+      value: scholarity,
+      onChanged: controller.setScholarityFiliation1,
+    );
+  }
+}
+
+class _JobFilliation1Field extends StatelessWidget {
+  const _JobFilliation1Field({
+    Key? key,
+    this.job,
+    required this.controller,
+  }) : super(key: key);
+
+  final String? job;
+  final EnrollmentFiliationBloc controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return TagTextField(
+      key: const Key('STUDENT_ENROLLMENT_FILLIATION_FILLIATION1_JOB'),
+      label: "Profissão",
+      hint: "Digite a profissão",
+      onChanged: controller.setJobFiliation1,
+      value: job,
+    );
+  }
+}
+
+// Filliation 2
+
+class _NameFilliation2Field extends StatelessWidget {
+  const _NameFilliation2Field({
+    Key? key,
+    this.name,
+    required this.controller,
+  }) : super(key: key);
+
+  final String? name;
+  final EnrollmentFiliationBloc controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return TagTextField(
+      key: const Key('STUDENT_ENROLLMENT_FILLIATION_FILLIATION2_NAME'),
+      label: "Nome",
+      hint: "Nome completo",
+      onChanged: controller.setNameFiliation2,
+      value: name,
+      validator: requiredValidator,
+    );
+  }
+}
+
+class _CpfFilliation2Field extends StatelessWidget {
+  const _CpfFilliation2Field({
+    Key? key,
+    this.cpf,
+    required this.controller,
+  }) : super(key: key);
+
+  final String? cpf;
+  final EnrollmentFiliationBloc controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return TagTextField(
+      key: const Key('STUDENT_ENROLLMENT_FILLIATION_FILLIATION2_CPF'),
+      label: "CPF",
+      hint: "digite o CPF",
+      onChanged: controller.setCpfFiliation2,
+      formatters: [TagMasks.maskCPF],
+      value: cpf,
+      validator: requiredValidator,
+    );
+  }
+}
+
+class _RGFilliation2Field extends StatelessWidget {
+  const _RGFilliation2Field({
+    Key? key,
+    this.rg,
+    required this.controller,
+  }) : super(key: key);
+
+  final String? rg;
+  final EnrollmentFiliationBloc controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return TagTextField(
+      key: const Key('STUDENT_ENROLLMENT_FILLIATION_FILLIATION2_RG'),
+      label: "RG",
+      hint: "digite o número do RG",
+      onChanged: controller.setRgFiliation2,
+      value: rg,
+      validator: requiredValidator,
+    );
+  }
+}
+
+class _ScholarityFilliation2Field extends StatelessWidget {
+  const _ScholarityFilliation2Field({
+    Key? key,
+    this.scholarity,
+    required this.controller,
+  }) : super(key: key);
+
+  final int? scholarity;
+  final EnrollmentFiliationBloc controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return TagDropdownField(
+      key: const Key('STUDENT_ENROLLMENT_FILLIATION_FILLIATION2_SCHOLARITY'),
+      label: 'Escolaridade',
+      items: controller.scholarityItems,
+      value: scholarity,
+      onChanged: controller.setScholarityFiliation1,
+    );
+  }
+}
+
+class _JobFilliation2Field extends StatelessWidget {
+  const _JobFilliation2Field({
+    Key? key,
+    this.job,
+    required this.controller,
+  }) : super(key: key);
+
+  final String? job;
+  final EnrollmentFiliationBloc controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return TagTextField(
+      key: const Key('STUDENT_ENROLLMENT_FILLIATION_FILLIATION2_JOB'),
+      label: "Profissão",
+      hint: "Digite a profissão",
+      onChanged: controller.setJobFiliation2,
+      value: job,
+    );
   }
 }
