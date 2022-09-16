72e5d9300970eee1fe2b8e2fa8d67f7f7c066c78
Tori-IPTI <tori.ti@ipti.org.br>
2022-09-16 12:28:28 -0300

    Feito teste de list_cities_usercase_test, feito arquivo de coverage(fazer correções) iniciado arquivo de load_student_docs_usecase

---
+++ br_ipti_tag_app/test/app/features/student/domain/usecases/load_student_docs_usecase_test.dart
@@ -0,0 +1,11 @@
+import 'package:br_ipti_tag_app/app/features/student/domain/repositories/student_doc_address_repositories.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+class MockStudentRepository extends 
+Mock implements StudentDocumentsAddressRepository {}
+void main() {
+  testWidgets(
+      "LoadStudentDocsUsecase when load a right empty value", (tester) async {
+
+      });
+}
