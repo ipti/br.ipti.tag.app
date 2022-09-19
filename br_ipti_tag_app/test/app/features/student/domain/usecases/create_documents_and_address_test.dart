c9945b5b0473fcf5e97e3174395ae82db8684083
Tori-IPTI <tori.ti@ipti.org.br>
2022-09-19 13:12:51 -0300

    feito os testes de load_usecase e iniciado create_case

---
+++ br_ipti_tag_app/test/app/features/student/domain/usecases/create_documents_and_address_test.dart
@@ -0,0 +1,54 @@
+import 'dart:io';
+
+import 'package:br_ipti_tag_app/app/features/student/domain/entities/student_documents.dart';
+import 'package:br_ipti_tag_app/app/features/student/domain/repositories/student_doc_address_repositories.dart';
+import 'package:br_ipti_tag_app/app/features/student/domain/usecases/create_documents_and_address.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockStudentDocumentsAddressRepository extends Mock
+    implements StudentDocumentsAddressRepository {}
+
+void main() {
+  testWidgets(
+      "AddDocumentsAndAddressToStudentUsecase when create a right value",
+      (tester) async {
+    final studentDocsAddress = StudentDocsAddress(
+        schoolInepIdFk: "schoolInepIdFk",
+        studentFk: "studentFk",
+        rgNumber: "rgNumber",
+        edcensoUfFk: "edcensoUfFk",
+        edcensoCityFk: "edcensoCityFk");
+
+    final repository = MockStudentDocumentsAddressRepository();
+    when(() => repository.create(studentDocsAddress))
+        .thenAnswer((invocation) => Future.value(right(studentDocsAddress)));
+    final usercase = AddDocumentsAndAddressToStudentUsecase(repository);
+    final params = AddDocumentsAndAddressToStudentParams(
+        studentDocumentsAddress: studentDocsAddress);
+
+    final either = await usercase(params);
+    final result = either.fold(id, id);
+    expect(result, isA<StudentDocsAddress>());
+  });
+  testWidgets("AddDocumentsAndAddressToStudentUsecase when create a left value",
+      (tester) async {
+    final studentDocsAddress = StudentDocsAddress(
+        schoolInepIdFk: "schoolInepIdFk",
+        studentFk: "studentFk",
+        rgNumber: "rgNumber",
+        edcensoUfFk: "edcensoUfFk",
+        edcensoCityFk: "edcensoCityFk");
+    final repository = MockStudentDocumentsAddressRepository();
+    when(() => repository.create(studentDocsAddress)).thenAnswer((invocation) =>
+        Future.value(left(const SocketException("Ocorreu um erro"))));
+    final usercase = AddDocumentsAndAddressToStudentUsecase(repository);
+    final params = AddDocumentsAndAddressToStudentParams(
+        studentDocumentsAddress: studentDocsAddress);
+
+    final either = await usercase(params);
+    final result = either.fold(id, id);
+    expect(result, isA<SocketException>());
+  });
+}
--- br_ipti_tag_app/test/app/features/student/domain/usecases/load_student_docs_usecase_test.dart
+++ br_ipti_tag_app/test/app/features/student/domain/usecases/load_student_docs_usecase_test.dart
@@ -1,11 +1,46 @@
+import 'dart:io';
+
+import 'package:br_ipti_tag_app/app/features/student/domain/entities/student_documents.dart';
 import 'package:br_ipti_tag_app/app/features/student/domain/repositories/student_doc_address_repositories.dart';
+import 'package:br_ipti_tag_app/app/features/student/domain/usecases/load_student_docs_usecase.dart';
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
-class MockStudentRepository extends 
-Mock implements StudentDocumentsAddressRepository {}
-void main() {
-  testWidgets(
-      "LoadStudentDocsUsecase when load a right empty value", (tester) async {
 
-      });
+class MockStudentDocumentsAddressRepository extends Mock
+    implements StudentDocumentsAddressRepository {}
+
+void main() {
+  testWidgets("LoadStudentDocsUsecase when load a right value", (tester) async {
+    final repository = MockStudentDocumentsAddressRepository();
+    when(() => repository.getByStudentId('')).thenAnswer(
+      (realInvocation) => Future.value(right(StudentDocsAddress(
+          schoolInepIdFk: "schoolInepIdFk",
+          studentFk: "studentFk",
+          rgNumber: "rgNumber",
+          edcensoUfFk: "edcensoUfFk",
+          edcensoCityFk: "edcensoCityFk"))),
+    );
+    final usecase = LoadStudentDocsUsecase(repository);
+    final params =  LoadStudentDocsParams("", "");
+
+    final either = await usecase(params);
+    expect(either.isRight(), isTrue);
+
+    final result = either.fold(id, id);
+    expect(result, isA<StudentDocsAddress>());
+  });
+  testWidgets("LoadStudentDocsUsecase when load a left value", (tester) async {
+    final repository = MockStudentDocumentsAddressRepository();
+    when(() => repository.getByStudentId('')).thenAnswer(
+      (realInvocation) =>
+          Future.value(left(const SocketException("Ocorreu um erro"))),
+    );
+    final usecase = LoadStudentDocsUsecase(repository);
+    final params = LoadStudentDocsParams("", "");
+    final either = await usecase(params);
+    expect(either.isLeft(), isTrue);
+    final result = either.fold(id, id);
+    expect(result, isA<SocketException>());
+  });
 }
