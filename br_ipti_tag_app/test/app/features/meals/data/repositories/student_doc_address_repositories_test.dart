be39c82e03c3a79c53cd1edf8c059d30373a200d
Tori-IPTI <tori.ti@ipti.org.br>
2022-09-22 12:41:34 -0300

    feito teste de student_doc_adrress_repositories.dart

--- br_ipti_tag_app/test/app/features/meals/data/repositories/student_doc_address_repositories_test.dart
+++ br_ipti_tag_app/test/app/features/meals/data/repositories/student_doc_address_repositories_test.dart
@@ -1,27 +1,92 @@
-import 'package:br_ipti_tag_app/app/features/student/data/datasources/remote/student_doc_address_remote_datasource.dart';
-import 'package:br_ipti_tag_app/app/features/student/domain/entities/student_documents.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
 
-class MockStudentDocumentsAndAddressRemoteDataSource extends Mock 
-implements StudentDocumentsAndAddressRemoteDataSource {}
+import 'package:br_ipti_tag_app/app/features/student/data/datasources/remote/student_doc_address_remote_datasource.dart';
+import 'package:br_ipti_tag_app/app/features/student/data/models/student_docs_model.dart';
+import 'package:br_ipti_tag_app/app/features/student/data/repositories/student_docs_addrs_repository_impl.dart';
+import 'package:br_ipti_tag_app/app/features/student/domain/entities/student_documents.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockStudentDocumentsAndAddressRemoteDataSource extends Mock
+    implements StudentDocumentsAndAddressRemoteDataSource {}
 
 void main() {
-  final studentDocsAddress = StudentDocsAddress(
-      schoolInepIdFk: "schoolInepIdFk",
-      studentFk: "studentFk",
-      rgNumber: "rgNumber",
-      edcensoUfFk: "edcensoUfFk",
-      edcensoCityFk: "edcensoCityFk");
-  testWidgets("when StudentDocumentsAddressRepository has right value",
-      (tester) async {
-    final repository = MockStudentDocumentsAndAddressRemoteDataSource();
-    when(() => repository.getByStudentId(''))
-        .thenThrow((invocation) => Exception('Houve um problema'));
-    
-  
-    final params = studentDocsAddress;
+  group("When TagButton ", () {
+    final studentDocsAddressModel = StudentDocumentsAddressModel(
+        schoolInepIdFk: "schoolInepIdFk",
+        studentFk: "studentFk",
+        rgNumber: "rgNumber",
+        edcensoUfFk: "edcensoUfFk",
+        edcensoCityFk: "edcensoCityFk");
+    testWidgets("when getByStudentId has right value", (tester) async {
+      final datasource = MockStudentDocumentsAndAddressRemoteDataSource();
+      when(() => datasource.getByStudentId(''))
+          .thenAnswer((invocation) => Future.value(studentDocsAddressModel));
 
-    expect(params, isA<StudentDocsAddress>());
+      final repository = StudentDocumentsAddressRepositoryImpl(datasource);
+      final params = await repository.getByStudentId('');
+
+      expect(params.isRight(), isTrue);
+      final result = params.fold(id, id);
+      expect(result, isA<StudentDocsAddress>());
+    });
+    testWidgets("when getByStudentId has left value", (tester) async {
+      final datasource = MockStudentDocumentsAndAddressRemoteDataSource();
+      when(() => datasource.getByStudentId(''))
+          .thenThrow((invocation) => Exception("Não foi possível listar"));
+
+      final repository = StudentDocumentsAddressRepositoryImpl(datasource);
+      final params = await repository.getByStudentId('');
+
+      expect(params.isLeft(), isTrue);
+      final result = params.fold(id, id);
+      expect(result, isA<Exception>());
+    });
+    testWidgets("when create has right value", (tester) async {
+      final datasource = MockStudentDocumentsAndAddressRemoteDataSource();
+      when(() => datasource.create(studentDocsAddressModel))
+          .thenAnswer((invocation) => Future.value(studentDocsAddressModel));
+
+      final repository = StudentDocumentsAddressRepositoryImpl(datasource);
+      final params = await repository.create(studentDocsAddressModel);
+      expect(params.isRight(), isTrue);
+      final result = params.fold(id, id);
+      expect(result, isA<StudentDocsAddress>());
+    });
+    testWidgets("when create has left value", (tester) async {
+      final datasource = MockStudentDocumentsAndAddressRemoteDataSource();
+      when(() => datasource.create(studentDocsAddressModel)).thenThrow(
+          (invocation) => Exception("Não foi possível adicionar estudante"));
+
+      final repository = StudentDocumentsAddressRepositoryImpl(datasource);
+      final params = await repository.create(studentDocsAddressModel);
+
+      expect(params.isLeft(), isTrue);
+      final result = params.fold(id, id);
+      expect(result, isA<Exception>());
+    });
+    testWidgets("when update has right value", (tester) async {
+      final datasource = MockStudentDocumentsAndAddressRemoteDataSource();
+      when(() => datasource.update("", studentDocsAddressModel))
+          .thenAnswer((invocation) => Future.value(studentDocsAddressModel));
+
+      final repository = StudentDocumentsAddressRepositoryImpl(datasource);
+      final params = await repository.update("", studentDocsAddressModel);
+      expect(params.isRight(), isTrue);
+      final result = params.fold(id, id);
+      expect(result, isA<StudentDocsAddress>());
+    });
+    testWidgets("when alterar has left value", (tester) async {
+      final datasource = MockStudentDocumentsAndAddressRemoteDataSource();
+      when(() => datasource.update("", studentDocsAddressModel)).thenThrow(
+          (invocation) => Exception("Não foi possível alterar estudante"));
+
+      final repository = StudentDocumentsAddressRepositoryImpl(datasource);
+      final params = await repository.update("", studentDocsAddressModel);
+
+      expect(params.isLeft(), isTrue);
+      final result = params.fold(id, id);
+      expect(result, isA<Exception>());
+    });
   });
 }
